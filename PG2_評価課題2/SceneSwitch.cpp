d3a312268299419871e58cf3c96d9248d01647bb
konnitiwa4242@gmail.com <konnitiwa4242@gmail.com>
2024-01-22 12:44:32 +0900

    シーン切り替えようにクラスを作成しました。

---
+++ PG2_評価課題2/SceneSwitch.cpp
@@ -0,0 +1,50 @@
+﻿#include "SceneSwitch.h"
+
+SceneSwitch::SceneSwitch() {
+	scene      = title;
+	preScene   = scene;
+	mainScene  = new MainScene;
+	titleScene = new TitleScene;
+}
+
+SceneSwitch::~SceneSwitch() {
+	delete mainScene;
+	delete titleScene;
+}
+
+void SceneSwitch::Scene(char* keys, char* preKeys) {
+	preScene = scene;
+	if (scene == title) {
+		titleScene->TitleDraw();
+	}
+	else if (scene == game) {
+		mainScene->MainLoop(keys,preKeys);
+	}
+}
+
+void SceneSwitch::Movement(char* keys, char* preKeys) {
+	if (scene == title && keys[DIK_RETURN] && !preKeys[DIK_RETURN]) {
+		scene = game;
+	}
+	else if (scene == game && keys[DIK_RETURN] && !preKeys[DIK_RETURN]) {
+		scene = title;
+	}
+}
+
+void SceneSwitch::Ini() {
+	if (scene !=preScene ) {
+		if (scene == title) {
+			titleScene = new TitleScene;
+		}
+		else if (scene == game) {
+			mainScene = new MainScene;
+		}
+	}
+	preScene = scene;
+}
+
+void SceneSwitch::Update(char* keys, char* preKeys) {
+	Ini();
+	Scene(keys, preKeys);
+	Movement(keys, preKeys);
+}
---
+++ PG2_評価課題2/SceneSwitch.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+#include "TitleScene.h"
+#include "MainScene.h"
+enum Scene {
+	title,
+	game,
+	end,
+};
+class SceneSwitch
+{
+private:
+	Scene scene;
+	Scene preScene;
+
+	TitleScene* titleScene;
+	MainScene*  mainScene;
+public:
+
+	/// <summary>
+	/// コンストラクター
+	/// </summary>
+	SceneSwitch();
+
+	/// <summary>
+	/// デストラクター
+	/// </summary>
+	~SceneSwitch();
+
+	/// <summary>
+	/// シーン切り替え
+	/// </summary>
+	void Scene(char *keys,char *preKeys);
+
+	/// <summary>
+	/// キー入力することでのシーン切り替え
+	/// </summary>
+	/// <param name="keys"></param>
+	/// <param name="preKeys"></param>
+	void Movement(char* keys, char* preKeys);
+
+	/// <summary>
+	/// 更新処理の初期化
+	/// </summary>
+	void Ini();
+
+	/// <summary>
+	/// 上記の関数をまとめた
+	/// </summary>
+	void Update(char* keys, char* prekeys);
+};
+
